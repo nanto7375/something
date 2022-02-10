fbb1a9cb169a16533ecac6cd1a76882bd79e8db2
정승우 <seungwoo@jeongseung-us-MacBook-Air.local>
2022-02-11 01:55:41 +0900

    first

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+		commonjs: true,
+		browser: true,
+		es6: true,
+  },
+	settings: {"import/resolver": { "node": {"extensions": [".js", ".ts"] }}},
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    "indent": [ "error", 2 ],
+    "space-in-parens": [ "error", "always", { "exceptions": [ "{}", "[]"] }],
+    "object-curly-spacing": [ "error", "always" ],
+    "object-curly-newline": [ "error", { "multiline": true } ],
+    "array-bracket-spacing": [ "error", "always" ],
+    "comma-dangle": [ "error", { "arrays": "always-multiline", "objects": "always-multiline", "imports": "never", "exports": "never", "functions": "never" } ],
+    "max-len": ["error", { "code": 500 }],
+    "semi": [ "error", "always" ],
+    "eol-last": ["error", "always"],
+  },
+};
